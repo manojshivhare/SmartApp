596dbb22bab1e8a4c966003fd064815812b31196
Manoj Shivhare <manojshivhare820@gmail.com>
2020-05-15 16:02:25 +0530

    started designing UI

---
+++ SmartAppDemo/SmartAppDemo/View/LatestMovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LatestMovieCollectionViewCell.swift
+//  SmartAppDemo
+//
+//  Created by Manoj Shivhare on 15/05/20.
+//  Copyright © 2020 Manoj Shivhare. All rights reserved.
+//
+
+import UIKit
+
+class LatestMovieCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ SmartAppDemo/SmartAppDemo/View/TopRatedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TopRatedCollectionViewCell.swift
+//  SmartAppDemo
+//
+//  Created by Manoj Shivhare on 15/05/20.
+//  Copyright © 2020 Manoj Shivhare. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedCollectionViewCell: UICollectionViewCell {
+    
+}
